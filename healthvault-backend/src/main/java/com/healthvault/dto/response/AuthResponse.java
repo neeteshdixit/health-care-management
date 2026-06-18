dfd6d878ed7ba198571f7684fd4bc9ef0b7e7878
neeteshdixit <dixitneetesh857@gmail.com>
2026-06-18 23:15:43 +0530

    feat: implement JWT authentication and spring security config

---
+++ healthvault-backend/src/main/java/com/healthvault/dto/response/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.healthvault.dto.response;
+
+import com.healthvault.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String email;
+    private Role role;
+    private String message;
+}
